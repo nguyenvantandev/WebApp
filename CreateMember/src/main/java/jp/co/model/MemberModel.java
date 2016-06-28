c4256410a84e1fbfb017929915ab5d906de6bff1
Nguyen Van Tan <tan.nguyen@fois.co.jp>
2016-06-28 11:27:16 +0900

    更新

---
+++ CreateMember/src/main/java/jp/co/model/MemberModel.java
@@ -0,0 +1,78 @@
+package jp.co.model;
+
+public class MemberModel {
+	private String memeber_id;
+	private String member_name;
+	private String sex;
+	private String year;
+	private String month;
+	private String day;
+	private String birthplace;
+	private String mari;
+	private String employee;
+	private String persent_address;
+
+	public String getMemeber_id() {
+		return memeber_id;
+	}
+	public void setMemeber_id(String memeber_id) {
+		this.memeber_id = memeber_id;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getBirthplace() {
+		return birthplace;
+	}
+	public void setBirthplace(String birthplace) {
+		this.birthplace = birthplace;
+	}
+	public String getMari() {
+		return mari;
+	}
+	public void setMari(String mari) {
+		this.mari = mari;
+	}
+	public String getEmployee() {
+		return employee;
+	}
+	public void setEmployee(String employee) {
+		this.employee = employee;
+	}
+	public String getPersent_address() {
+		return persent_address;
+	}
+	public void setPersent_address(String persent_address) {
+		this.persent_address = persent_address;
+	}
+	
+	
+	
+}
